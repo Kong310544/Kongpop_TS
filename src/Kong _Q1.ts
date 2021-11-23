9dbd27df392442f5c4c54868c5305b2ad9f0d05c
Kong310544 <kong310544@gmail.com>
2021-11-23 13:46:52 +0700

    first commit

---
+++ src/Kong _Q1.ts
@@ -0,0 +1,9 @@
+let Employees : [number, string, number, number, number,number][];
+
+Employees = [
+    [1, "Kongpop", 2, 0, 0,0],
+    [2, "Jakrit", 3, 0, 0,0],
+    [3, "Chalermchai", 5, 0, 0,0],
+    [4, "Chawanwit", 2, 0, 0,0],
+    [5, "Thanakorn", 1, 0, 0,0]
+];
